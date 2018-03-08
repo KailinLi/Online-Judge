b3ce841580d9f8df53414191b73a930ed6d1dad6
KailinLi <likailinhust@gmail.com>
2018-03-08 21:53:09 +0800

    03/08/2018

--- UVa/12563. Jin Ge Jin Qu hao(2D).cc
+++ UVa/12563. Jin Ge Jin Qu hao(2D).cc
@@ -8,7 +8,7 @@ using namespace std;
 const int MAXN = 50 + 5;
 int l[MAXN];
 const int MAXM = 180 * 50 + 678 + 5;
-int dp[MAXM];
+int dp[MAXN][MAXM];
 int t, n, s;
 int main () {
 	std::ios::sync_with_stdio(false);
@@ -21,22 +21,23 @@ int main () {
 		scanf("%d %d", &n, &s);
 		REP(i, n) scanf("%d", l + i);
 		memset(dp, -1, sizeof(dp));
-		int time = 0;
-		dp[0] = 0;
+		REP(i, n + 1) dp[i][0] = 0;
 		for (int i = 1; i <= n; ++i) {
-			for (int j = s - 1; j >= l[i - 1]; --j) {
-				if (dp[j - l[i - 1]] != -1)
-					dp[j] = max(dp[j], dp[j - l[i - 1]] + 1);
+			for (int j = 0; j < s; ++j) {
+				if (dp[i - 1][j] != -1) dp[i][j] = dp[i - 1][j];
+				if (j - l[i - 1] >= 0 && dp[i - 1][j - l[i - 1]] != -1)
+					dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - l[i - 1]] + 1);
 			}
 		}
-		int ans = -1;
-		for (int i = s - 1; i > 0; --i) {
-			if (dp[i] != -1 && (ans == -1 || dp[ans] < dp[i]))
-				ans = i;
+		int res = -1, t_i = 0;
+		for (int j = s - 1; j >= 0; --j) {
+			for (int i = 1; i <= n; ++i) {
+				if (dp[i][j] != -1 && (res == -1 || dp[t_i][res] < dp[i][j])) {
+					res = j;
+					t_i = i;
+				}
+			}
 		}
-		if (ans != -1)
-			printf("Case %d: %d %d\n", ++kase, dp[ans] + 1, ans + 678);
-		else
-			printf("Case %d: %d %d\n", ++kase, 1, 678);
+		printf("Case %d: %d %d\n", ++kase, dp[t_i][res] + 1, res + 678);
 	}
 }
