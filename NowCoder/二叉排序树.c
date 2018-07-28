f4bee3ebe6f241ed2bdd1289a42b5ddf1bc9ca54
李恺林 <likailinhust@gmail.com>
2018-07-28 14:41:13 +0800

    07/28/2018

--- NowCoder/二叉排序树.c
+++ NowCoder/二叉排序树.c
@@ -4,44 +4,71 @@
 
 #define REP(i, n) for (int (i) = 0; (i) < (n); ++(i))
 #define INF 0x3f3f3f3f
-
-typedef struct node {
+typedef struct _node {
     int val;
-    struct node *l;
-    struct node *r;
+    struct _node *l;
+    struct _node *r;
 } Node;
+Node *new_node(int v) {
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->val = v;
+    n->l = n->r = NULL;
+    return n;
+}
 int n;
-Node *tree = NULL;
+void preorder(Node *h) {
+    if (!h) return;
+    printf("%d ", h->val);
+    preorder(h->l);
+    preorder(h->r);
+}
+void inorder(Node *h) {
+    if (!h) return;
+    inorder(h->l);
+    printf("%d ", h->val);
+    inorder(h->r);
+}
+void postorder(Node *h) {
+    if (!h) return;
+    postorder(h->l);
+    postorder(h->r);
+    printf("%d ", h->val);
+    free(h);
+}
 
-int main () {
+int main (void) {
 #ifdef LOCAL
     freopen("input.txt", "r", stdin);
 #endif
-    scanf("%d", &n);
-    while (n--) {
+    while (scanf("%d", &n) != EOF) {
+        Node *head = NULL;
         int i;
-        scanf("%d", &i);
-        Node *pa = NULL;
-        Node *rt = tree;
-        while (rt) {
-            pa = rt;
-            if (i > pa->val)
-                rt = rt->r;
-            else
-                rt = rt->l;
+        while (n--) {
+            scanf("%d", &i);
+            Node *p = head;
+            Node *new_n = new_node(i);
+            while (p) {
+                if (i > p->val) {
+                    if (p->r) p = p->r;
+                    else {
+                        p->r = new_n;
+                        break;
+                    }
+                }
+                else if (i < p->val) {
+                    if (p->l) p = p->l;
+                    else {
+                        p->l = new_n;
+                        break;
+                    }
+                }
+                else break;
+            }
+            if (!p) head = new_n;
         }
-        rt = (Node *)malloc(sizeof(Node));
-        rt->val = i;
-        rt->l = rt->r = NULL;
-        if (!pa) {
-            printf("-1\n");
-            tree = rt;
-        }
-        else {
-            printf("%d\n", pa->val);
-            if (i > pa->val) pa->r = rt;
-            else pa->l = rt;
-        }
+        preorder(head); printf("\n");
+        inorder(head); printf("\n");
+        postorder(head); printf("\n");
     }
     return 0;
 }
