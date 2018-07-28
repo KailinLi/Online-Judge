e7d52e66d1061449d9505e7ab4da1659a1b61461
李恺林 <likailinhust@gmail.com>
2018-07-28 13:49:33 +0800

    07/28/2018

--- NowCoder/a+b.c
+++ NowCoder/a+b.c
@@ -4,61 +4,35 @@
 
 #define REP(i, n) for (int (i) = 0; (i) < (n); ++(i))
 #define INF 0x3f3f3f3f
-#define MAXN 1000 + 5
-int a[MAXN];
-int b[MAXN];
-int a_length;
-int b_length;
-int res[MAXN];
-int res_length;
-char input;
-int state;
+#define MAXN (15)
 
-void fuc() {
-    int over = 0;
-    int i = a_length - 1;
-    int j = b_length - 1;
-    while (i >= 0 && j >= 0) {
-        int sum = a[i] + b[j] + over;
-        over = sum / 10;
-        res[res_length++] = sum % 10;
-        --i;
-        --j;
+char input[MAXN];
+
+int get_num() {
+    int a = 0;
+    int flg = 0;
+    char *pch = strtok(input, ",");
+    while (pch) {
+        a *= 1000;
+        a += atoi(pch);
+        if (a < 0) {
+            a = -a;
+            flg = 1;
+        }
+        pch = strtok(NULL, ",");
     }
-    while (i >= 0) {
-        int sum = a[i] + over;
-        over = sum / 10;
-        res[res_length++] = sum % 10;
-        --i;
-    }
-    while (j >= 0) {
-        int sum = b[j] + over;
-        over = sum / 10;
-        res[res_length++] = sum % 10;
-        --j;
-    }
-    if (over) printf("%d", over);
-    for (int i = res_length - 1; i >= 0; --i)
-        printf("%d", res[i]);
-    printf("\n");
-    a_length = b_length = res_length = 0;
-    state = 0;
+    return (flg) ? -a : a;
 }
 
-int main () {
+int main (void) {
 #ifdef LOCAL
     freopen("input.txt", "r", stdin);
 #endif
-    while (scanf("%c", &input) != EOF) {
-        if ((input == '\n' || input == ' ') && a_length && b_length)
-            fuc();
-        else if ((input == '\n' || input == ' ') && a_length && !b_length)
-            state = 1;
-        else if (state)
-            b[b_length++] = input - '0';
-        else
-            a[a_length++] = input - '0';
+    while (scanf("%s", input) != EOF) {
+        int a = get_num();
+        scanf("%s", input);
+        int b = get_num();
+        printf("%d\n", a + b);
     }
-    if (a_length && b_length) fuc();
     return 0;
 }
