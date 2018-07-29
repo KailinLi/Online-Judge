2e05be7a40a6696bca3544af188eaf6e2e5c4297
李恺林 <likailinhust@gmail.com>
2018-07-29 15:10:21 +0800

    07/29/2018

--- CCF_CSP/201312/最大的矩形(n^2).cc
+++ CCF_CSP/201312/最大的矩形(n^2).cc
@@ -2,14 +2,13 @@
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
-#include <climits>
 
 #define REP(i, n) for (int (i) = 0; (i) < (n); ++(i))
 using namespace std;
 const int INF = 0x3f3f3f3f;
 const int MAXN = 1000 + 5;
+int h[MAXN];
 int n;
-int max_h[MAXN][MAXN];
 
 int main (void) {
     // std::ios::sync_with_stdio(false);
@@ -17,16 +16,13 @@ int main (void) {
     freopen("input.txt", "r", stdin);
 #endif
     scanf("%d", &n);
-    REP(i, n) scanf("%d", &max_h[i][i]);
+    REP(i, n) scanf("%d", h + i);
+    int s = -INF;
     REP(i, n) {
-        for (int j = i + 1; j < n; ++j) {
-            max_h[i][j] = min(max_h[i][j - 1], max_h[j][j]);
-        }
-    }
-    int s = INT_MIN;
-    REP(i, n) {
+        int max_h = h[i];
         for (int j = i; j < n; ++j) {
-            s = max(s, max_h[i][j] * (j - i + 1));
+            max_h = min(max_h, h[j]);
+            s = max(s, max_h * (j - i + 1));
         }
     }
     printf("%d\n", s);
