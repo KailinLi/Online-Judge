73c3031676f723ab56d46674a8cc0bf02b25fff0
李恺林 <likailinhust@gmail.com>
2018-09-10 18:54:10 +0800

    09/10/2018

--- UVa/247. Calling Circles.cc
+++ UVa/247. Calling Circles.cc
@@ -2,76 +2,67 @@
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
-#include <map>
-#include <string>
 
 #define REP(i, n) for (int (i) = 0; (i) < (n); ++(i))
 using namespace std;
+// const int INF = 0x3f3f3f3f;
 const int MAXN = 25 + 5;
 int dp[MAXN][MAXN];
-int v[MAXN];
+char name[MAXN][MAXN];
+char buf[MAXN];
+int pos;
 int n, m;
-int id;
-map<string, int> name;
-int getID(const string &s) {
-	map<string, int>::iterator f = name.find(s);
-	if (f == name.end()) {
-		return name[s] = id++;
-	}
-	else {
-		return f->second;
-	}
+int get_id() {
+    int i = 0;
+    while (i < pos) {
+        if (!strcmp(buf, name[i])) break;
+        ++i;
+    }
+    if (i == pos) {
+        strcpy(name[pos], buf);
+        return pos++;
+    }
+    else return i;
 }
-char s1[100], s2[100];
-int kase;
+int vis[MAXN];
 void dfs(int u) {
-	v[u] = 1;
-	REP(i, n) {
-		if (!v[i] && dp[i][u] && dp[u][i]) {
-			for (map<string, int>::iterator t = name.begin(); t != name.end(); ++t) {
-				if (t->second == i) {
-					printf(", %s", t->first.c_str());
-					break;
-				}
-			}
-			dfs(i);
-		}
-	}
+    printf("%s", name[u]);
+    REP(i, n) {
+        if (dp[u][i] && dp[i][u] && !vis[i]) {
+            printf(", ");
+            vis[i] = 1;
+            dfs(i);
+        }
+    }
 }
-int main () {
-	std::ios::sync_with_stdio(false);
+int kase;
+
+int main (void) {
+    // std::ios::sync_with_stdio(false);
 #ifdef LOCAL
-	freopen("input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
-	while (scanf("%d %d", &n, &m) == 2 && n) {
-		id = 0;
-		name.clear();
-		memset(dp, 0, sizeof(dp));
-		memset(v, 0, sizeof(v));
-		REP(i, n)
-			dp[i][i] = 1;
-		while (m--) {
-			scanf("%s %s", s1, s2);
-			dp[getID(s1)][getID(s2)] = 1;	// use const string & !!
-		}
-		REP(k, n)
-			REP(i, n)
-				REP(j, n)
-					dp[i][j] |= dp[i][k] && dp[k][j];
-		if (kase) printf("\n");
-		printf("Calling circles for data set %d:\n", ++kase);
-		REP(i, n) {
-			if (!v[i]) {
-				for (map<string, int>::iterator t = name.begin(); t != name.end(); ++t) {
-					if (t->second == i) {
-						printf("%s", t->first.c_str());
-						break;
-					}
-				}
-				dfs(i);
-				printf("\n");
-			}
-		}
-	}
-	return 0;
+    while (scanf("%d %d", &n, &m) == 2 && (n || m)) {
+        pos = 0;
+        memset(dp, 0, sizeof(dp));
+        memset(vis, 0, sizeof(vis));
+        REP(i, n) dp[i][i] = 1;
+        while (m--) {
+            scanf("%s", buf);
+            int u = get_id();
+            scanf("%s", buf);
+            int v = get_id();
+            dp[u][v] = 1;
+        }
+        REP(k, n) REP(i, n) REP(j, n) dp[i][j] |= dp[i][k] && dp[k][j];
+        if (kase) printf("\n");
+        printf("Calling circles for data set %d:\n", ++kase);
+        REP(i, n) if (!vis[i]) {
+            vis[i] = 1;
+            dfs(i);
+            printf("\n");
+        }
+    }
+    return 0;
 }
